26b49c1a66415f53bf4268c7e3a08b17419b07ac
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2025-06-11 20:03:51 -0300

    feat: add trainers and services routes to the main router

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import userRoutes from "../features/user/user.routes";
+import trainersRoutes from "../features/trainers/trainers.routes";
+import servicesRoutes from "../features/services/services.routes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/trainers", trainersRoutes);
+router.use("/services", servicesRoutes);
 
 export default router;
