3ec6554274dfa2221ef901a2c07b874e3ff7c3c7
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2025-06-17 16:53:25 -0300

    feat: add hiring, reviews and archives endpoints

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -2,11 +2,17 @@ import { Router } from "express";
 import userRoutes from "../features/user/user.routes";
 import trainersRoutes from "../features/trainers/trainers.routes";
 import servicesRoutes from "../features/services/services.routes";
+import hiringRoutes from "../features/hiring/hiring.routes";
+import reviewsRoutes from "../features/reviews/reviews.routes";
+import archivesRoutes from "../features/archives/archives.routes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/trainers", trainersRoutes);
 router.use("/services", servicesRoutes);
+router.use("/hirings", hiringRoutes);
+router.use("/reviews", reviewsRoutes);
+router.use("/archives", archivesRoutes);
 
 export default router;
